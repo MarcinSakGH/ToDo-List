d18c040f8abd48b9719cd10d8420965f4a32ebc4
Marcin <marcin.sak.of@gmail.com>
2024-11-06 11:07:06 +0100

    Add Task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    PENDING = 'pending'
+    COMPLETED = 'completed'
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (COMPLETED, 'Completed')
+    )
+    
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=PENDING
+    )
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateField(default=timezone.now)
+    completed_at = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    
+    
+    def mark_as_completed(self):
+        """Change task status to 'completed' and set completion date""" 
+        self.status = self.COMPLETED
+        self.completed_at = timezone.now().date()
+        self.save()
+      
